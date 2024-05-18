92e88705756cb7481d579f095550b15d03f8807c
juanjcsr <juanjcsr@gmail.com>
2024-05-18 21:45:03 +0100

    add simpleserver app

---
+++ apps/simpleserver/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	log.Println("starting server on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	log.Println("received request")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+
+}
