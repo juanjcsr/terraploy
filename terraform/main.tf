800ec5e06638b51045cf194f9fba9aa3fc2a63fc
juanjcsr <juanjcsr@gmail.com>
2024-05-19 21:40:39 +0100

    add finalizer to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,13 @@
 
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
+
 
 resource "helm_release" "argocd" {
   name       = "argocd"
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  namespace  = kubernetes_namespace.argocd.metadata.0.name
+  namespace  = "argocd"
+  create_namespace = true
 
   values = [file("../charts/argo-cd/values.yaml")]
 }
@@ -20,6 +17,20 @@ resource "helm_release" "argocd_config" {
     depends_on = [helm_release.argocd]
     name = "argocd-config"
     chart = "../charts/root-app"
+    timeout = 30
+}
+
+resource "null_resource" "clean_up_argocd_resources" {
+  provisioner "local-exec" {
+    command     = <<-EOT
+      kubeconfig=~/.kube/config
+      
+      kubectl --kubeconfig $kubeconfig get Application -A -o name | xargs -I {} kubectl --kubeconfig $kubeconfig -n argocd patch -p '{"metadata":{"finalizers":null}}' --type=merge {} 2> /tmp/tf.clean_up_argocd_resources.err.log || true
+      #rm -f $kubeconfig
+    EOT
+    interpreter = ["bash", "-c"]
+    when        = destroy
+  }
 }
 
 
