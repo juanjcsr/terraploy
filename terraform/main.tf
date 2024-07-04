62ce9f7c5544a87cc65335a7014970a708212c62
Juan Carlos <juan.sanchez@loveholidays.com>
2024-07-04 12:33:59 +0100

    add linkerd and disable aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 
-module "awscloud" {
-  source = "./modules/awscloud"
+# module "awscloud" {
+#   source = "./modules/awscloud"
 
-  ecr_repo_name = var.ecr_repo_name
+#   ecr_repo_name = var.ecr_repo_name
 
-  ecr_repos_names = var.ecr_repos_names
+#   ecr_repos_names = var.ecr_repos_names
 
-  github_oidc_repo = var.github_oidc_repo
-}
+#   github_oidc_repo = var.github_oidc_repo
+# }
 
 module "kubernetes" {
   source = "./modules/kubernetes"
